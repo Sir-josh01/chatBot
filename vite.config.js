543173477a7fd1a0f00c1fbfcb8d4fff750d2db7
Sir-josh <docsmile01@gmail.com>
2025-11-03 00:49:44 +0100

    added a slash to the base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'react_chatBot',
+  base: '/react_chatBot',
   server: {
     port: 3000,
   }
